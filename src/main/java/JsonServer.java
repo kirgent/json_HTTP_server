ebd4dc2e3bb5ccafd049f09cb554fc2258904e23
kirgent <kirgent@gmail.com>
2018-05-22 14:45:53 +0300

    update

--- src/main/java/JsonServer.java
+++ src/main/java/JsonServer.java
@@ -20,61 +20,122 @@ public class JsonServer {
     private static final int PORT = 8080;
     private static final int BACKLOG = 1;
 
-    private static final Charset CHARSET = StandardCharsets.UTF_8;
+    static final Charset CHARSET = StandardCharsets.UTF_8;
 
-    private static final int STATUS_OK = 200;
-    private static final int STATUS_NOT_FOUND = 404;
-    private static final int STATUS_METHOD_NOT_ALLOWED = 405;
-
+    static int STATUS_OK = 200;
+    private static int STATUS_METHOD_NOT_ALLOWED = 405;
+    private static int STATUS_NOT_FOUND = 404;
     private static final int NO_RESPONSE_LENGTH = -1;
 
     private static final String HEADER_ALLOW = "Allow";
-    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    static String HEADER_CONTENT_TYPE = "Content-Type";
+
     private static final String METHOD_GET = "GET";
     private static final String METHOD_POST = "POST";
     private static final String METHOD_OPTIONS = "OPTIONS";
-    private static final String ALLOWED_METHODS = METHOD_GET + "," + METHOD_OPTIONS;
+    private static final String ALLOWED_METHODS = METHOD_GET + "," + METHOD_POST + "," + METHOD_OPTIONS;
 
-    static ArrayList list_config_params = new ArrayList();
+    private static List<String> test;
+    private static List macaddress;
+    private static List count_reminders;
+    private static List<String> count_iterations;
+
 
     public static void main(final String... args) throws IOException {
-        common_api c = new common_api();
+        common_api common = new common_api();
         final HttpServer server = HttpServer.create(new InetSocketAddress(HOSTNAME, PORT), BACKLOG);
 
-        list_config_params = c.read_config();
-        c.write_config("app.name", "1231111111");
-        list_config_params = c.read_config();
+        System.out.println(common.read_config());
+        common.write_config("app.name", "1111111111111");
+        System.out.println(common.read_config());
 
-        System.out.println("app.name=" + list_config_params.get(0));
-        System.out.println("app.version=" + list_config_params.get(1));
-        System.out.println("app.vendor=" + list_config_params.get(2));
-
-        server.createContext("/func2", he -> {
+        server.createContext("/", httpExchange -> {
             try {
-                final Headers headers = he.getResponseHeaders();
-                final String requestMethod = he.getRequestMethod().toUpperCase();
-                switch (requestMethod) {
+                final Headers headers = httpExchange.getResponseHeaders();
+                switch (httpExchange.getRequestMethod().toUpperCase()) {
                     case METHOD_GET:
-                        final Map<String, List<String>> requestParameters = getRequestParameters(he.getRequestURI());
-                        System.out.println("Does URL contain key test2? : " + requestParameters.containsKey("test2"));
+
+                        //<a href="URL">текст_ссылки</a>
+
+                        String example = "Example of reminders request: http://localhost:8080/reminders?" +
+                            "test=test1&" +
+                            "macaddress=123456789012&" +
+                            "count_reminders=X&" +
+                            "count_iterations=Y";
+                        String s1 = "List of tests for NewAPI:\n" +
+                                "test1_Add_Purge, " +
+                                "test2_Add_Delete_Purge, " +
+                                "test3_Add_Modify_Delete_Purge\n\n" +
+                                "List of tests for OldAPI:\n" +
+                                "test1_Add_Purge, " +
+                                "test2_Add_Delete_Purge";
+
+
+                        String responseBody = example + "\n" + s1;
+
+                        final Map<String, List<String>> requestParameters = getRequestParameters(httpExchange.getRequestURI());
                         // do something with the request parameters
-                        final String responseBody = "['TEST2 - hello world!']";
-                        headers.set(HEADER_CONTENT_TYPE, String.format("application/json; charset=%s", CHARSET));
-                        final byte[] rawResponseBody = responseBody.getBytes(CHARSET);
-                        he.sendResponseHeaders(STATUS_OK, rawResponseBody.length);
-                        he.getResponseBody().write(rawResponseBody);
+
+                        common.write_to_file(responseBody);
+                        common.write_response(httpExchange, headers, responseBody);
                         break;
                     case METHOD_OPTIONS:
+                        System.out.println("case METHOD_OPTIONS !!!");
                         headers.set(HEADER_ALLOW, ALLOWED_METHODS);
-                        he.sendResponseHeaders(STATUS_OK, NO_RESPONSE_LENGTH);
+                        httpExchange.sendResponseHeaders(STATUS_OK, NO_RESPONSE_LENGTH);
                         break;
                     default:
+                        System.out.println("case default !!!");
                         headers.set(HEADER_ALLOW, ALLOWED_METHODS);
-                        he.sendResponseHeaders(STATUS_METHOD_NOT_ALLOWED, NO_RESPONSE_LENGTH);
+                        httpExchange.sendResponseHeaders(STATUS_METHOD_NOT_ALLOWED, NO_RESPONSE_LENGTH);
                         break;
                 }
             } finally {
-                he.close();
+                httpExchange.close();
+            }
+        });
+        server.createContext("/reminders", httpExchange -> {
+            try {
+                final Headers headers = httpExchange.getResponseHeaders();
+                //System.out.println(headers.get(HEADER_CONTENT_TYPE).toString());
+                final String requestMethod = httpExchange.getRequestMethod().toUpperCase();
+                switch (requestMethod) {
+                    case METHOD_GET:
+                        final Map<String, List<String>> requestParameters = getRequestParameters(httpExchange.getRequestURI());
+                        // do something with the request parameters
+                        test = requestParameters.get("test");
+                        macaddress = requestParameters.get("macaddress");
+                        count_reminders = requestParameters.get("count_reminders");
+                        count_iterations = requestParameters.get("count_iterations");
+
+                        String list_params = "test=" + test +
+                                ", macaddress=" + macaddress +
+                                ", count_reminders="+ count_reminders +
+                                ", count_iterations=" + count_iterations;
+
+                        /*if(macaddress.length()>12){
+                            System.out.println("Wrong macaddress !!!" + macaddress.length());
+                        }
+                        if(!count_reminders.getClass().equals("Integer")){
+                            System.out.println("Wrong count_reminders type !!!" + count_reminders.getClass());
+                        }*/
+                        String html = "<html><body><h1>Testing of reminders</h1></body></html>";
+
+                        final String responseBody = html + list_params;
+
+                        common.write_response(httpExchange, headers, responseBody);
+                        break;
+                    case METHOD_OPTIONS:
+                        headers.set(HEADER_ALLOW, ALLOWED_METHODS);
+                        httpExchange.sendResponseHeaders(STATUS_OK, NO_RESPONSE_LENGTH);
+                        break;
+                    default:
+                        headers.set(HEADER_ALLOW, ALLOWED_METHODS);
+                        httpExchange.sendResponseHeaders(STATUS_METHOD_NOT_ALLOWED, NO_RESPONSE_LENGTH);
+                        break;
+                }
+            } finally {
+                httpExchange.close();
             }
         });
         server.start();
--- src/main/java/common_api.java
+++ src/main/java/common_api.java
@@ -1,8 +1,12 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+
+import javax.security.auth.login.Configuration;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Properties;
 
-class common_api {
+class common_api extends JsonServer{
 
     ArrayList read_config() throws IOException {
         ArrayList list = new ArrayList();
@@ -14,18 +18,49 @@ class common_api {
         list.add(1, p.getProperty("app.version", "default_version"));
         list.add(2, p.getProperty("app.vendor","Java"));
 
-        System.out.println("app.name=" + list.get(0));
-        System.out.println("app.version=" + list.get(1));
-        System.out.println("app.vendor=" + list.get(2));
-
+        //System.out.println("app.name=" + list.get(0));
+        //System.out.println("app.version=" + list.get(1));
+        //System.out.println("app.vendor=" + list.get(2));
         return list;
         }
 
         void write_config(String key, String value) throws IOException {
-            Properties p = new Properties();
-            String fileName= "src/main/resources/config.properties";
-            p.load(new FileInputStream(fileName));
-            p.setProperty(key, value);
+            try {
+                Properties p = new Properties();
+                p.setProperty(key, value);
+
+                FileOutputStream os = new FileOutputStream(new File("src/main/resources/config.properties"));
+                p.store(os, "Favorite Things");
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
         }
+
+    void write_to_file(String result) throws IOException {
+        FileWriter writer = new FileWriter("ServerLog.txt", true);
+        writer.write(result);
+        writer.append('\n');
+        writer.flush();
+    }
+
+    void write_response(HttpExchange httpExchange, Headers headers, String responseBody) throws IOException {
+        /*String basic_response = "HTTP/1.1 200 OK\r\n" +
+                "Server: JsonServer 0.1\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: " + responseBody.length() + "\r\n" +
+                "Connection: close\r\n\r\n";*/
+        System.out.println(responseBody);
+        //headers.set(HEADER_CONTENT_TYPE, String.format("application/json; charset=%s", CHARSET));
+        headers.set(HEADER_CONTENT_TYPE, String.format("application/json; charset=%s", CHARSET));
+        //headers.set("HTTP/1.1", "200 OK");
+        headers.set("Server", "JsonServer 0.1");
+        headers.set("Content-Length", responseBody.toString());
+        //headers.set("Connection", "close");
+        final byte[] rawResponseBody = responseBody.getBytes(CHARSET);
+        httpExchange.sendResponseHeaders(STATUS_OK, rawResponseBody.length);
+        httpExchange.getResponseBody().write(rawResponseBody);
+    }
 }
