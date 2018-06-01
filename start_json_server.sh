08d622fc5f83aa733b6cd964d65f98660a6070c9
kirill.grushin <kirgent@gmail.com>
2018-06-01 17:43:48 +0300

    auto commit

--- start_json_server.sh
+++ start_json_server.sh
@@ -3,34 +3,3 @@ java -Dfile.encoding=UTF-8 -classpath \
 ./target/classes:\
 ./libs/* \
 json_server
-
-
-#/usr/lib/jvm/icedtea-bin-8/bin/java -javaagent
-#/home/kirill.grushin/idea-IC-182.2574.2/lib/idea_rt.jar=45225
-#/home/kirill.grushin/idea-IC-182.2574.2/bin -Dfile.encoding=UTF-8 -classpath /usr/lib/jvm/icedtea-bin-8/jre/lib/charsets.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/cldrdata.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/dnsns.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/jaccess.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/localedata.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/nashorn.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/sunec.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/sunjce_provider.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/sunpkcs11.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/ext/zipfs.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/jce.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/jsse.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/management-agent.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/resources.jar
-#/usr/lib/jvm/icedtea-bin-8/jre/lib/rt.jar
-#/home/kirill.grushin/IdeaProjects/json_HTTP_server/target/classes
-#/home/kirill.grushin/.m2/repository/org/junit/jupiter/junit-jupiter-api/5.2.0/junit-jupiter-api-5.2.0.jar
-#/home/kirill.grushin/.m2/repository/org/apiguardian/apiguardian-api/1.0.0/apiguardian-api-1.0.0.jar
-#/home/kirill.grushin/.m2/repository/org/opentest4j/opentest4j/1.1.0/opentest4j-1.1.0.jar
-#/home/kirill.grushin/.m2/repository/org/junit/platform/junit-platform-commons/1.2.0/junit-platform-commons-1.2.0.jar
-#/home/kirill.grushin/.m2/repository/org/apache/httpcomponents/httpclient/4.5.5/httpclient-4.5.5.jar
-#/home/kirill.grushin/.m2/repository/org/apache/httpcomponents/httpcore/4.4.9/httpcore-4.4.9.jar
-#/home/kirill.grushin/.m2/repository/commons-codec/commons-codec/1.10/commons-codec-1.10.jar
-#/home/kirill.grushin/.m2/repository/com/googlecode/json-simple/json-simple/1.1.1/json-simple-1.1.1.jar
-#/home/kirill.grushin/.m2/repository/org/hamcrest/hamcrest-core/1.1/hamcrest-core-1.1.jar
-#/home/kirill.grushin/.m2/repository/org/json/json/20180130/json-20180130.jar
-#/home/kirill.grushin/.m2/repository/commons-logging/commons-logging/1.2/commons-logging-1.2.jar json_server
